343e9b8bacc993dfd5577bcdbaa2c387cfc0a618
Isaias Pomales <impomales@gmail.com>
2019-01-15 15:40:32 -0500

    define a dispatcher for incoming intents

--- fitBotLambda/index.js
+++ fitBotLambda/index.js
@@ -1,8 +1,17 @@
-exports.handler = async event => {
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!')
+const {QueryFood} = require('./intentTypes')
+const {handleQueryFood} = require('./intentHandlers')
+
+function dispatch(request) {
+  const intentName = request.currentIntent.name
+
+  switch (intentName) {
+    case QueryFood:
+      return handleQueryFood(request)
+    default:
+      throw Error(`Intent with name ${intentName} not supported.`)
   }
-  return response
+}
+
+exports.handler = async request => {
+  return dispatch(request)
 }

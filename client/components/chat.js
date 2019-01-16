a83f05e9332599cf3e24a63cff6375fc34682496
Isaias Pomales <impomales@gmail.com>
2019-01-16 11:59:52 -0500

    define class method to connect chat to lex bot on the front end

--- client/components/chat.js
+++ client/components/chat.js
@@ -1,10 +1,23 @@
+import axios from 'axios'
 import React, {Component} from 'react'
 import ChatHistory from './chat-history'
 
 export class Chat extends Component {
+  componentDidMount() {
+    this.initializeBot()
+  }
+
+  initializeBot() {
+    axios
+      .post('/api/bot/initiate', {option: 'LEX'})
+      .then(() => console.log('Bot has been successfully initiated.'))
+      .catch(err => console.error(err))
+  }
+
   render() {
     return (
       <div>
+        {/* can add a switch setting later to change bot through UI */}
         <ChatHistory />
         <form>
           <input type="text" />

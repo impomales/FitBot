2083f48ad88b37c890746c1efea706af24fb1a47
Isaias Pomales <impomales@gmail.com>
2019-01-16 10:37:24 -0500

    define route to send a message to bot

--- server/api/bot.js
+++ server/api/bot.js
@@ -1,19 +1,53 @@
 const router = require('express').Router()
+
 const aws = require('aws-sdk')
 
 // bot options
 const LEX = 'LEX'
 // const WATSON = 'WATSON';
 
-let bot
+let bot, botOption, sessionUserId
 
-// expects botOption in req that sets which bot to use.
+// expects option in req that sets which bot to use.
 router.post('/initiate', async (req, res, next) => {
-  const {botOption} = req.body
+  const {option} = req.body
 
-  if (botOption === LEX) {
+  if (option === LEX) {
+    botOption = option
     bot = new aws.LexRuntime({region: 'us-east-1'})
     res.json(bot)
+  } else {
+    const err = new Error('Invalid bot option')
+    next(err)
+  }
+})
+
+router.post('/message', (req, res, next) => {
+  const {text} = req.body
+
+  if (!bot) {
+    const err = new Error('Bot has not been initialized.')
+    next(err)
+  }
+
+  if (botOption === LEX) {
+    // user data is currently hard coded.
+    // TODO link currently logged in user's data to session attributes.
+    bot.postText(
+      {
+        botAlias: process.env.BOT_ALIAS,
+        botName: process.env.BOT_NAME,
+        userId: '2-isaias',
+        inputText: text,
+        sessionAttributes: {
+          userGoals: '1200'
+        }
+      },
+      (err, response) => {
+        if (err) next(err)
+        else res.json(response)
+      }
+    )
   } else {
     const err = new Error('Invalid bot option')
     next(err)

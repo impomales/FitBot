5079118efdb01ec52a6f2e39f72c62bbc9904ce6
Isaias Pomales <impomales@gmail.com>
2019-01-17 11:22:07 -0500

    add e2e test of logging in and sending a 'hi' message to bot

--- tests/index.js
+++ tests/index.js
@@ -6,7 +6,21 @@ module.exports = {
       .getTitle(function(title) {
         browser.expect(title).to.equal('Fitbot')
       })
-      // TODO test user log in.
+      // test user log in.
+      .setValue('input[name=email]', 'imp@email.com')
+      .setValue('input[name=password]', '12345')
+      .click('button')
+      // test chat ui.
+      .waitForElementVisible('#chat-main', 1000)
+      .setValue('.inputField', 'hi')
+      .submitForm('#chat-main form')
+      .pause(1000)
+      .getText('#chat-history li:last-child', function(element) {
+        console.log(element.value)
+        browser
+          .expect(element.value)
+          .to.equal('Hello, I am your assistant Fitbot. How can I help you?')
+      })
       .end()
   }
 }

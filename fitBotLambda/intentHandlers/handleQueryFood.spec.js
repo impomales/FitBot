94d47aae3813c8204c97464df2ab67607e754db0
Isaias Pomales <impomales@gmail.com>
2019-01-18 12:50:03 -0500

    define test for handling QueryFood intent

--- fitBotLambda/intentHandlers/handleQueryFood.spec.js
+++ fitBotLambda/intentHandlers/handleQueryFood.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 const {expect} = require('chai')
 const {
   handleQueryFood,
@@ -6,31 +7,31 @@ const {
 } = require('./handleQueryFood')
 
 describe('Handle Query Food', () => {
-  const milkInfo = {
-    foods: [
-      {
-        food_name: 'milk',
-        serving_qty: 1,
-        serving_unit: 'cup',
-        serving_weight_grams: 245,
-        nf_calories: 124.95
-      }
-    ]
-  }
-
-  const carrotInfo = {
-    foods: [
-      {
-        food_name: 'carrot',
-        serving_qty: 1,
-        serving_unit: 'carrot',
-        serving_weight_grams: '46',
-        nf_calories: 16.1
-      }
-    ]
-  }
-
   describe('helper functions', () => {
+    const milkInfo = {
+      foods: [
+        {
+          food_name: 'milk',
+          serving_qty: 1,
+          serving_unit: 'cup',
+          serving_weight_grams: 245,
+          nf_calories: 124.95
+        }
+      ]
+    }
+
+    const carrotInfo = {
+      foods: [
+        {
+          food_name: 'carrot',
+          serving_qty: 1,
+          serving_unit: 'carrot',
+          serving_weight_grams: '46',
+          nf_calories: 16.1
+        }
+      ]
+    }
+
     it('can build a food query', () => {
       expect(buildFoodQuery('apple', 1, 'cup')).to.equal('1 cup of apple')
       expect(buildFoodQuery('apple', 2, 'cup')).to.equal('2 cups of apple')
@@ -39,11 +40,9 @@ describe('Handle Query Food', () => {
     })
 
     it('can build a food query result', () => {
+      // when serving unit is specified.
       expect(buildFoodQueryResult(milkInfo, 'cup')).to.equal(
         '1 cup of milk has 124.95 calories.'
-      )
-      expect(buildFoodQueryResult(carrotInfo)).to.equal(
-        '1 carrot has 16.1 calories.'
       )
 
       milkInfo.foods[0].serving_qty = 5
@@ -52,11 +51,44 @@ describe('Handle Query Food', () => {
         '5 cups of milk has 624.75 calories.'
       )
 
+      // when serving unit is not specified.
+      expect(buildFoodQueryResult(carrotInfo)).to.equal(
+        '1 carrot has 16.1 calories.'
+      )
       carrotInfo.foods[0].serving_qty = 10
       carrotInfo.foods[0].nf_calories *= 10
       expect(buildFoodQueryResult(carrotInfo)).to.equal(
         '10 carrots have 161 calories.'
       )
+    })
+  })
+
+  describe('handleQueryFood', () => {
+    const request = {
+      currentIntent: {
+        slots: {
+          FoodQueryName: 'apple',
+          FoodQueryQuantity: null,
+          FoodQueryUnit: null
+        },
+        name: 'QueryFood'
+      },
+      invocationSource: 'DialogCodeHook',
+      sessionAttributes: {},
+      inputTranscript: 'How many calories are in an apple?'
+    }
+    it("can handle when user specifies a quantity using 'a' or 'an'", () => {
+      const result = handleQueryFood(request)
+      expect(result.dialogAction.type).to.equal('Delegate')
+      expect(result.dialogAction.slots.FoodQueryQuantity).to.equal('1')
+    })
+    // test fulfillment hook
+    it('can process a fulfillment hook', async () => {
+      request.invocationSource = 'FulfillmentCodeHook'
+      const result = await handleQueryFood(request)
+      expect(result.dialogAction.type).to.equal('ConfirmIntent')
+      expect(result.dialogAction.intentName).to.equal('LogFood')
+      expect(result.dialogAction.slots.Calories).to.not.be.null
     })
   })
 })

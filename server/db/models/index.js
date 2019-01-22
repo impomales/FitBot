8ba4eac584db0c77b9c6449670a6930b83026a99
Isaias Pomales <impomales@gmail.com>
2019-01-22 12:15:34 -0500

    define food log model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,5 @@
 const User = require('./user')
+const FoodLog = require('./foodLog')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -6,6 +7,8 @@ const User = require('./user')
  *
  *    BlogPost.belongsTo(User)
  */
+FoodLog.belongsTo(User)
+User.hasMany(FoodLog)
 
 /**
  * We'll export all of our models here, so that any time a module needs a model,
@@ -14,5 +17,6 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User,
+  FoodLog
 }

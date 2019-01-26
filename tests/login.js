b7415be4283dee43bf0b36126e2b6eedd829aa59
Isaias Pomales <impomales@gmail.com>
2019-01-26 00:18:59 -0500

    test response against array of acceptable responses

--- tests/login.js
+++ tests/login.js
@@ -1,13 +1,9 @@
-const {loginStep, sendMessage} = require('../testHelpers')
+const {loginStep, sendMessage, responsesToHi} = require('../testHelpers')
 
 module.exports = {
   'Loggin in, and typing hi': function(browser) {
     loginStep(browser)
-    sendMessage(
-      browser,
-      'hi',
-      'Hello, I am your assistant Fitbot. How can I help you?'
-    )
+    sendMessage(browser, 'hi', responsesToHi)
     browser.end()
   }
 }

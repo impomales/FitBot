482762596a211d8e84e4baf8e69142d082b0cc12
Isaias Pomales <impomales@gmail.com>
2019-01-30 13:52:33 -0500

    write end2end test for when user denies a confirmation

--- tests/queryFood.js
+++ tests/queryFood.js
@@ -10,7 +10,14 @@ module.exports = {
     sendMessage(browser, 'breakfast', ['Your banana has been logged.'])
   },
   'Querying food, and replying no to confirmation': function(browser) {
-    // TODO implement later.
+    sendMessage(browser, 'how many calories are in  1 cup of rice', [
+      '1 cup of rice have 205.4 calories. Would you like to log this item?'
+    ])
+    sendMessage(browser, 'no', [
+      'Ok. This item has not been logged.',
+      'Alright, I will not log it.',
+      "OK. I won't log"
+    ])
     browser.end()
   }
 }

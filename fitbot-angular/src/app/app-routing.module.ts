01b1990d9626519fa2aa4eb5dd322e39d57e1fe7
Isaias Pomales <impomales@gmail.com>
2019-02-11 16:45:56 -0500

    add basic routing between pages

--- fitbot-angular/src/app/app-routing.module.ts
+++ fitbot-angular/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core'
+import {Routes, RouterModule} from '@angular/router'
 
-const routes: Routes = [];
+import {ChatComponent} from './chat/chat.component'
+import { AuthFormsComponent } from './auth-forms/auth-forms.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/chat', pathMatch: 'full'},
+  {path: 'chat', component: ChatComponent},
+  {path: 'login', component: AuthFormsComponent},
+  {path: 'signup', component: AuthFormsComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

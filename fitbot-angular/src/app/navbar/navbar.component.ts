5c745ca8bae6b949d17cf04d3e42b39dd9ae268e
Isaias Pomales <impomales@gmail.com>
2019-02-12 11:05:36 -0500

    conditionally display nav links

--- fitbot-angular/src/app/navbar/navbar.component.ts
+++ fitbot-angular/src/app/navbar/navbar.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  logout() {
+    this.isLoggedIn = !this.isLoggedIn
+  }
 }

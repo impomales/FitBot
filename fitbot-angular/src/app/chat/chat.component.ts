e085c20a527ca39b147c72083912314d0264cddd
Isaias Pomales <impomales@gmail.com>
2019-02-18 10:51:29 -0500

    a user can send bot a message and receive a response

--- fitbot-angular/src/app/chat/chat.component.ts
+++ fitbot-angular/src/app/chat/chat.component.ts
@@ -1,6 +1,11 @@
 import {Component, OnInit} from '@angular/core'
 import {AuthService} from '../auth/auth.service'
-import {ChatService, Bot, InitBotResponse} from './chat.service'
+import {ChatService} from './chat.service'
+
+export interface Message {
+  type: String
+  content: String
+}
 
 @Component({
   selector: 'app-chat',
@@ -11,6 +16,7 @@ export class ChatComponent implements OnInit {
   busy: Boolean = false
   text: string = ''
   option: string = ''
+  messages: Message[] = []
 
   constructor(
     public authService: AuthService,
@@ -23,8 +29,46 @@ export class ChatComponent implements OnInit {
 
   initializeBot() {
     this.chatService.initializeBot(this.option).subscribe(
-      (response: InitBotResponse) => {
-        console.log(response)
+      ({bot}) => {
+        this.messages.push(
+          {
+            type: 'status',
+            content: `You are now chatting with ${bot.type}`
+          },
+          {
+            type: 'received',
+            content: `${bot.type.toLowerCase()}: Hello, I am the ${
+              bot.type
+            } Fitbot. How can I help you?`
+          }
+        )
+
+        this.option = bot.type
+      },
+      err => console.error(err)
+    )
+  }
+
+  handleSubmit() {
+    const {text, option} = this
+
+    const sent = {
+      type: 'sent',
+      content: this.text
+    }
+
+    this.busy = true
+    this.messages.push(sent)
+    this.text = ''
+
+    this.chatService.messageBot(text, option).subscribe(
+      data => {
+        this.messages.push({
+          type: 'received',
+          content: `${option.toLowerCase()}: ${data.message}`
+        })
+        this.busy = false
+        console.log(this.messages)
       },
       err => console.error(err)
     )
--- fitbot-angular/src/app/chat/chat.service.ts
+++ fitbot-angular/src/app/chat/chat.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core'
 import {HttpClient, HttpHeaders} from '@angular/common/http'
 import {Observable} from 'rxjs'
 import {AuthService} from '../auth/auth.service'
+import {tap} from 'rxjs/operators'
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -9,12 +10,16 @@ const httpOptions = {
   })
 }
 
-export class InitBotResponse {
+export interface InitBotResponse {
   sessionUserId: string
   bot: Bot
 }
 
-export class Bot {
+export interface MessageBotResponse {
+  message: string
+}
+
+export interface Bot {
   type: string
 }
 
@@ -22,12 +27,21 @@ export class Bot {
   providedIn: 'root'
 })
 export class ChatService {
+  sessionUserId: String
+
   constructor(private http: HttpClient, private authService: AuthService) {}
+
   initializeBot(option: string): Observable<InitBotResponse> {
-    console.log(this.authService.user)
-    return this.http.post<InitBotResponse>(
-      '/api/bot/initiate',
-      {option},
+    return this.http
+      .post<InitBotResponse>('/api/bot/initiate', {option}, httpOptions)
+      .pipe(tap(response => (this.sessionUserId = response.sessionUserId)))
+  }
+
+  messageBot(text: string, option: string): Observable<MessageBotResponse> {
+    const {sessionUserId} = this
+    return this.http.post<MessageBotResponse>(
+      '/api/bot/message',
+      {text, sessionUserId, option},
       httpOptions
     )
   }

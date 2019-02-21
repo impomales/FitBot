27941ccf0e794fd872c4bbfe6162469a9a3bdb56
Isaias Pomales <impomales@gmail.com>
2019-02-21 16:01:09 -0500

    add test for logging in and sending a message

--- fitbot-angular/e2e/src/app.e2e-spec.ts
+++ fitbot-angular/e2e/src/app.e2e-spec.ts
@@ -1,25 +1,37 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import {AppPage, responsesToHi} from './app.po'
+import {browser, logging} from 'protractor'
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+  let page: AppPage
 
   beforeEach(() => {
-    page = new AppPage();
-  });
+    page = new AppPage()
+  })
 
   it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Fitbot');
-  });
+    page.navigateTo()
+    expect(page.getTitleText()).toEqual('Fitbot')
+  })
 
-  it('can log in a user, and user can type \'hi\'')
+  it("can log in a user, and user can type 'hi'", async () => {
+    page.navigateTo()
+    page.login()
+    expect(browser.getCurrentUrl()).toEqual(`${browser.baseUrl}/chat`)
+
+    const result = await page.sendMessage('hi', responsesToHi)
+    expect(result).toBeTruthy()
+  })
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    }));
-  });
-});
+    const logs = await browser
+      .manage()
+      .logs()
+      .get(logging.Type.BROWSER)
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE
+      })
+    )
+  })
+})
--- fitbot-angular/e2e/src/app.po.ts
+++ fitbot-angular/e2e/src/app.po.ts
@@ -1,15 +1,39 @@
-import { browser, by, element } from 'protractor';
+import {browser, by, element} from 'protractor'
+
+export const responsesToHi = [
+  'Hello, I am your assistant Fitbot. How can I help you?',
+  'Greetings, I am Fitbot! How can I assist?',
+  'Good day! I am Fitbot. What can I do for you today?',
+  'Hello! How can I help you?',
+  'Hi! How are you doing?',
+  'Hello, I am your assistant Fitbot. How can I help you?'
+]
 
 export class AppPage {
   navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+    return browser.get(browser.baseUrl) as Promise<any>
   }
 
   getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+    return element(by.css('app-root h1')).getText() as Promise<string>
   }
 
   login() {
-    
+    element(by.css('input[name=email]')).sendKeys('cody@email.com')
+    element(by.css('input[name=password')).sendKeys('123')
+    return element(by.css('button')).click() as Promise<void>
+  }
+
+  async sendMessage(message: string, responses: string[]) {
+    element(by.css('.input-field')).sendKeys(message)
+    element(by.css('#chat-main form')).submit()
+
+    return element(by.css('#chat-history li:last-child'))
+      .getText()
+      .then(lastMessage => {
+        return responses.some(response =>
+          lastMessage.toLowerCase().includes(response.toLowerCase())
+        ) as boolean
+      })
   }
 }

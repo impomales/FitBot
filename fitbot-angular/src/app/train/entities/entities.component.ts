c540c1c9d68ac3f08becfae56031ecc448e5fb01
Isaias Pomales <impomales@gmail.com>
2019-03-05 17:10:26 -0500

    can delete enitities

--- fitbot-angular/src/app/train/entities/entities.component.ts
+++ fitbot-angular/src/app/train/entities/entities.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit, DoCheck} from '@angular/core'
 import {Entity} from './entity.model'
 import {EntityService} from './entity.service'
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {FormGroup, FormControl, Validators} from '@angular/forms'
+import {Router, ActivatedRoute} from '@angular/router'
 
 @Component({
   selector: 'app-entities',
@@ -11,20 +12,35 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class EntitiesComponent implements OnInit {
   entities: Entity[]
   createEntityForm: FormGroup
+  paramId: string
 
-  constructor(private entityService: EntityService) {}
+  constructor(
+    private entityService: EntityService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.getEntities()
     this.createEntityForm = new FormGroup({
-      'name': new FormControl(null, Validators.required)
+      name: new FormControl(null, Validators.required)
     })
   }
 
   onSubmit() {
     // TODO don't allow duplicates
-    this.entities.push({name: this.createEntityForm.get('name').value, values: []})
+    this.entities.push({
+      name: this.createEntityForm.get('name').value,
+      values: []
+    })
     this.createEntityForm.reset()
+  }
+
+  deleteEntity(index: number) {
+    // TODO delete annotations in intents.
+    this.entities[index] = null
+    this.entities.splice(index, 1)
+    this.paramId = this.router.url.split('/')[3]
+    if (+this.paramId === index) this.router.navigate(['/train/entities'])
   }
 
   getEntities() {

8e7be487e82d2359f6ec1ed9030abed96df74a3f
Isaias Pomales <impomales@gmail.com>
2019-03-13 16:16:53 -0400

    define exercise model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,10 +1,15 @@
 const User = require('./user')
 const FoodLog = require('./foodLog')
+const ExerciseLog = require('./exerciseLog')
 
 FoodLog.belongsTo(User)
+ExerciseLog.belongsTo(User)
+
 User.hasMany(FoodLog)
+User.hasMany(ExerciseLog)
 
 module.exports = {
   User,
-  FoodLog
+  FoodLog,
+  ExerciseLog
 }

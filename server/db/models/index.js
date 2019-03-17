39f09895c2f331a64ede6d1bb0345dd55a492e63
Isaias Pomales <impomales@gmail.com>
2019-03-17 14:24:46 -0400

    define a workout model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,12 +1,15 @@
 const User = require('./user')
 const FoodLog = require('./foodLog')
 const ExerciseLog = require('./exerciseLog')
+const Workout = require('./workout')
 
 FoodLog.belongsTo(User)
 ExerciseLog.belongsTo(User)
+ExerciseLog.belongsTo(Workout)
 
 User.hasMany(FoodLog)
 User.hasMany(ExerciseLog)
+Workout.hasMany(ExerciseLog)
 
 module.exports = {
   User,
